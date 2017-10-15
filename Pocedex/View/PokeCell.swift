bb4be833bdbb46d0ca1401d48951f64799ce7b18
Ruslan Akberov <ruslanakberov@gmail.com>
2017-10-15 23:22:18 +0300

    PokeCell created

---
+++ Pocedex/View/PokeCell.swift
@@ -0,0 +1,25 @@
+//
+//  PokeCell.swift
+//  Pocedex
+//
+//  Created by Руслан Акберов on 15.10.2017.
+//  Copyright © 2017 Ruslan Akberov. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        thumbImg.image = UIImage(named: String(self.pokemon.pokedexId))
+        nameLbl.text = self.pokemon.name.capitalized
+    }
+    
+    
+}
